208acda9726f6da56b44f87dd7d5c84c8e962d64
Akshay <akshayc_118@hotmail.com>
2026-05-02 00:38:01 +0000

    adding pod setup script

--- pod_setup.sh
+++ pod_setup.sh
@@ -3,5 +3,7 @@ apt-get update
 apt-get install zstd
 apt-get install lshw
 curl -fsSL https://ollama.com/install.sh | sh
+git config --global user.email "akshayc_118@hotmail.com"
+git config --global user.name "Akshay"
 echo "ready"
 echo "ollama serve & -- ollama pull llama3.2"
